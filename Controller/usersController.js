f7d18a0d517c5ce7f1c36db528140285b47a8bc7
Benjamin-Re <renker.benjamin@gmail.com>
2022-06-26 13:37:20 +0200

    Add get one user route

--- Controller/usersController.js
+++ Controller/usersController.js
@@ -4,9 +4,27 @@ const Model = require('../Model/userModel');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-// Get all documents from collection
+// Get all users from collection
 router.get('/', (req, res)=>{
     Model.find().then((data) => {
+        res.status(200).json({
+            status: 'suceeded',
+            data,
+            error: null
+        })
+    }).catch((error) => {
+        res.status(404).json({
+            status: 404,
+            data,
+            error: error.message
+        })
+    })
+})
+
+// Get one user from collection
+router.get('/:id', authenticateToken, (req, res)=>{
+    let id = req.params.id;
+    Model.findById(id).then((data) => {
         res.status(200).json({
             status: 'suceeded',
             data,
@@ -67,8 +85,8 @@ router.post('/login', (req, res)=>{
     })
 })
 
-// Patch document
-router.patch('/:id', (req, res)=>{
+// Patch user, include jwt check middleware
+router.patch('/:id', authenticateToken, (req, res)=>{
     let id = req.params.id;
     let data = req.body;
     let options = { new: true }; // true to return the updated obj not original
@@ -90,15 +108,16 @@ router.patch('/:id', (req, res)=>{
 })
 
 // middleware to check if user is logged in
-// function authenticateToken(req, res, next) {
-//     const authHeader = req.headers['authorization'];
-//     const token = authHeader && authHeader.split(' ')[1];
-//     if(token===null)return res.sendStatus(401);
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-//         if(err) return res.sendStatus(403);
-//         req.user = user;
-//         next();
-//     })
-// }
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token===null)return res.sendStatus(401);
+    // authData is the credentials
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, authData) => {
+        if(err) return res.sendStatus(403);
+        req.authData = authData;
+        next();
+    })
+}
 
 module.exports = router;
